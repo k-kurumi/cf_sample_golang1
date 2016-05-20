6ab861964c978b50530c97fc1dc0a1c94fee1cfe
k-kurumi <optpia.kurumi@gmail.com>
2016-05-20 13:54:44 +0900

    ログ出力確認

--- main.go
+++ main.go
@@ -1,14 +1,39 @@
 package main
 
 import (
-	"github.com/kataras/iris"
+	"fmt"
 	"os"
+	"runtime"
+
+	"github.com/kataras/iris"
 )
 
+// 並列数の制限
+// http://qiita.com/ruiu/items/d4ef16981b870a1b85af
+
+// このWaitGroup方式ではメモリが溢れた
+// http://deeeet.com/writing/2014/07/30/golang-parallel-by-cpu/
+
+var cpus int
+
 func main() {
-	iris.Get("/hi", func(ctx *iris.Context) {
-		page := map[string]interface{}{"Name": "Iris"}
-		ctx.Render("hi.html", page)
+	cpus := runtime.NumCPU()
+
+	iris.Get("/", func(ctx *iris.Context) {
+
+		c := make(chan bool, cpus)
+		for i := 1; i <= 500; i++ {
+			c <- true
+
+			go func(i int) {
+				defer func() { <-c }()
+				fmt.Println("mylog", i)
+			}(i)
+
+		}
+
+		ctx.Write("cpus:", cpus)
 	})
+
 	iris.Listen(":" + os.Getenv("PORT"))
 }
