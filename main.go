8ae3a0f325c29a3c887846966d9e55494589afb9
k-kurumi <optpia.kurumi@gmail.com>
2016-12-01 13:11:03 +0900

    HTTPステータスを返すように変更

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"os/exec"
 	"strconv"
 	"time"
 
@@ -21,6 +22,12 @@ func main() {
 	// 遅延して応答する
 	e.GET("/delay/:second", getDelay)
 
+	// pingする
+	e.GET("/ping/:address", getPing)
+
+	// 指定のHTTPステータスを返す
+	e.GET("/status/:code", getStatus)
+
 	e.Logger.Fatal(e.Start(":" + os.Getenv("PORT")))
 }
 
@@ -28,5 +35,25 @@ func main() {
 func getDelay(c echo.Context) error {
 	second, _ := strconv.Atoi(c.Param("second"))
 	time.Sleep(time.Duration(second) * time.Second)
-	return c.String(http.StatusOK, fmt.Sprintf("%d delay", second))
+	return c.String(http.StatusOK, fmt.Sprintf("delay: %d sec", second))
+}
+
+// 外部コマンド実行
+func getPing(c echo.Context) error {
+	address := c.Param("address")
+	out, err := exec.Command("ping", "-c4", address).Output()
+	if err != nil {
+		return c.String(http.StatusBadRequest, err.Error())
+	}
+	return c.String(http.StatusOK, string(out))
+}
+
+// 指定されたHTTPステータスを返す
+func getStatus(c echo.Context) error {
+	code, err := strconv.Atoi(c.Param("code"))
+	if err != nil {
+		return c.String(http.StatusBadRequest, err.Error())
+	}
+
+	return c.String(code, "")
 }
